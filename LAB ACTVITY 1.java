a48a51a1461f489a4df7a92be4cf679913b0e2c1
Shaeey <159915956+Shaeey@users.noreply.github.com>
2024-02-13 21:33:24 +0800

    Add files via upload

---
+++ LAB ACTVITY 1.java
@@ -0,0 +1,38 @@
+
+package shane;
+
+import java.util.Scanner;
+
+public class Shane {
+    
+    static Scanner str = new Scanner(System.in);
+    static Scanner num = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    
+
+        System.out.print("Enter name: ");
+        String name = str.nextLine();
+        
+        System.out.print("Enter age: ");
+        int age = num.nextInt();
+        
+        System.out.print("Enter gender: ");
+        String gen = str.nextLine();
+        
+        System.out.print("Enter address: ");
+        String addr = str.nextLine();
+        
+        System.out.print("Enter civil status: ");
+        String CS = str.nextLine();
+        
+        System.out.print("Enter no. siblings: ");
+        int nsib = num.nextInt();
+        
+        
+        
+        
+        
+    }
+    
+}
