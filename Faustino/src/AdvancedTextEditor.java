e7aa69d860a2f3b36a2c99b950c247b8abc98cd4
Shaeey <159915956+Shaeey@users.noreply.github.com>
2024-03-17 21:18:30 +0800

    lab activity 2.3

---
+++ Faustino/src/AdvancedTextEditor.java
@@ -0,0 +1,29 @@
+
+import java.util.Stack;
+public class AdvancedTextEditor extends TextEditor {
+
+    private final Stack<String> history;
+
+    public AdvancedTextEditor(String text) {
+        super(text);
+        history = new Stack<>();
+    }
+
+    public void undo() {
+        if (!history.isEmpty()) {
+            setText(history.pop());
+        }
+    }
+
+    @Override
+    public void append(String newText) {
+        history.push(getText());
+        super.append(newText);
+    }
+
+    @Override
+    public void delete(int n) {
+        history.push(getText());
+        super.delete(n);
+    }
+}
